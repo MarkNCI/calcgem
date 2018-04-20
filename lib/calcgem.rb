4321c8193a2d753a44f2e509490739a87d061563
marknci <m.arkhayden@hotmail.com>
2018-04-20 23:19:09 +0000

    second commit

--- lib/calcgem.rb
+++ lib/calcgem.rb
@@ -1,5 +1,14 @@
 require "calcgem/version"
 
 module Calcgem
-  # Your code goes here...
+  def self.check(num)
+ 	if num<18 
+ 		result = "Cost is $25"
+ 	elsif num>=18 && num<60
+ 		result = "Cost is $50"
+ 	else
+ 		result = "Cost is free"
+ 	end
+ 	return result
+  end
 end
